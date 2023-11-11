1cf3f3bf542b04c512efbaf132cf4fc246fd8d3b
miahihi <myahihi39@gmail.com>
2023-11-12 01:25:18 +0700

    fix upload download file

--- Pages/Week/AddSubmission.cshtml.cs
+++ Pages/Week/AddSubmission.cshtml.cs
@@ -9,7 +9,7 @@ namespace Project.Pages.Week
     public class AddSubmissionModel : PageModel
     {
         public User userlogin { get; set; }
-        public Course c{ get; set; }
+        public Course c { get; set; }
         //public Models.Enrollment e { get; set; }
         public Assignment ass { get; set; }
         public WeekLesson wl { get; set; }
@@ -30,37 +30,80 @@ namespace Project.Pages.Week
                 }
             }
         }
-        public void OnPostUploadFile(IFormFile file, string assId)
+        public void OnPostUploadFile(IFormFile? file, IFormFile? refile, string assId)
         {
             using (prn231_finalprojectContext context = new prn231_finalprojectContext())
             {
-                //string loginID = HttpContext.Request.Cookies["loginId"];
+                string loginID = HttpContext.Request.Cookies["loginId"];
                 //ass = context.Assignments.FirstOrDefault(x => x.Id == int.Parse(assId));
                 //wl = context.WeekLessons.FirstOrDefault(x => x.Id == ass.Wlid);
                 //c = context.Courses.FirstOrDefault(x => x.CourseId == wl.CourseId);
                 //sm = context.Submissions.FirstOrDefault(x => x.AssignId == int.Parse(assId) && x.UserId == int.Parse(loginID));
-                if (file != null && file.Length > 0 && assId!=null)
+                if (file != null && file.Length > 0 && assId != null)
                 {
                     using (var memoryStream = new MemoryStream())
                     {
                         file.CopyTo(memoryStream);
 
                         // Lưu dữ liệu file vào cơ sở dữ liệu
-                        var curAssign = context.Assignments.FirstOrDefault(x=>x.Id==int.Parse(assId));
+                        var curAssign = context.Assignments.FirstOrDefault(x => x.Id == int.Parse(assId));
                         curAssign.Attachment = memoryStream.ToArray();
+
+
+                        Submission s = new Submission();
+                        s.AssignId = int.Parse(assId);
+                        s.UserId = int.Parse(loginID);
+                        s.LastModifiedTime = DateTime.Now;
+
+                        context.Submissions.Add(s);
+
                         context.SaveChanges();
                         ViewData["mess"] = "Upload file successfully!";
+                    }
+                }
+                else if (refile != null && refile.Length > 0 && assId != null)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        refile.CopyTo(memoryStream);
+
+                        // Lưu dữ liệu file vào cơ sở dữ liệu
+                        var curAssign = context.Assignments.FirstOrDefault(x => x.Id == int.Parse(assId));
+                        curAssign.Attachment = memoryStream.ToArray();
+
+
+                        Submission s = new Submission();
+                        s.AssignId = int.Parse(assId);
+                        s.UserId = int.Parse(loginID);
+                        s.LastModifiedTime = DateTime.Now;
+
+                        context.Submissions.Add(s);
+
+                        context.SaveChanges();
+                        ViewData["mess"] = "ReUpload file successfully!";
                     }
                 }
                 else
                 {
                     ViewData["mess"] = "Upload file Fail!";
                 }
+
                 OnGet(int.Parse(assId));
                 //return RedirectToPage("/Week/Detail", new { id = assId });
 
                 //ViewData["mess"] = "Upload file Fail!";
 
+            }
+        }
+        public IActionResult OnPostUnenroll(int courseId)
+        {
+            using (prn231_finalprojectContext context = new prn231_finalprojectContext())
+            {
+                string loginID = HttpContext.Request.Cookies["loginId"];
+                Enrollment data = context.Enrollments.FirstOrDefault(c => c.CourseId == courseId && c.UserId == int.Parse(loginID));
+                context.Enrollments.Remove(data);
+                context.SaveChanges();
+                return RedirectToPage("/Courses/Index");
             }
         }
     }
